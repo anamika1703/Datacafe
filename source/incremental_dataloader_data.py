6e2754ff2d89e7e427a60e3b30724dcc0c0523f3
anamika.sahu@accenture.com <anamika.sahu@accenture.com>
2022-08-25 09:37:35 +0000

    incremental pipeline changes

--- source/incremental_dataloader_data.py
+++ source/incremental_dataloader_data.py
@@ -25,18 +25,25 @@ source_data = read_config(SOURCE_PATH)
 expectationsdf = load_data("csv","dbfs:/FileStore/DataCafe/audit/event_log")
 expectationsdf = expectationsdf.filter(col("status") == 'COMPLETED').orderBy(col("date_timestamp").desc())
 last_date = expectationsdf.select("date_timestamp").collect()[0][0]
-modified_date = last_date + timedelta(days=1)
-modified_date = modified_date.strftime("%Y-%m-%d")
+now = datetime.now()
+if now != last_date:
+    modified_date = last_date + timedelta(days=1)
+    modified_date = modified_date.strftime("%Y-%m-%d %H:%M:%S")
+else:
+    modified_date = now + timedelta(hours=1)
+    modified_date = modified_date.strftime("%Y-%m-%d %H:%M:%S")
 last_successful_date = last_date.strftime("%Y-%m-%d")
 last_month = last_date.strftime("%m-%d")
+
+modified_date = modified_date.strftime("%Y-%m-%d %H:%M:%S")
+dt_string = now.strftime("%d%m%Y%H%M%S")
 
 for json_dict in source_data:
 
     if json_dict['stream_data'] == 'Y':
         source_df = load_data(json_dict['file_format'],json_dict['input_path'])
         source_df = time_conversion(source_df,"time")
-        input_data = source_df.where((source_df.date >= last_successful_date) & (source_df.date <= modified_date))
-        #input_data.show(5,False)
+        input_data = source_df.where((expectationsdf.date_timestamp >= modified_date) & (expectationsdf.date_timestamp <= last_successful_date))
         write_to_files(input_data,json_dict['file_format'],"dbfs:/FileStore/DataCafe/temp/")       
         file_path=glob.glob("/dbfs/FileStore/DataCafe/temp/*."+json_dict['file_format'])[0]
         file_path1=os.path.basename(file_path)
